e751bced90f22e0c60390ebfb02fcc6f5abc9b16
charlesoller <charlesrello@gmail.com>
2023-10-10 19:35:51 -0400

    finished

--- error-handling.js
+++ error-handling.js
@@ -1,26 +1,50 @@
-// 1.
-function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
-}
+// // 1.
+// function sum(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+//   }
+//   return sum;
+// }
 
-let res = sum(null);
-console.log(res);
+// try {
+//   let res = sum(null);
+//   console.log(res);
+// } catch (error){
+//   if (error instanceof TypeError){
+//     console.log(error.message)
+//   }
+// }
 
 // 2.
+function sayName(name){
+  if(typeof name !== "string"){
+    throw new TypeError("Invalid name! Must be a string!")
+  } else {
+    console.log(name);
+  }
+}
 // tests
-sayName("Alex");
-sayName(1);
+try {
+  sayName("Alex");
+  sayName(1);
+} catch (error) {
+  console.log(error.message)
+}
+
 // Your code here
 
-// 3.
-function greet(greeting) {
-  if (!greeting) {
-    throw new Error("There was no greeting given.");
-  }
+// // 3.
+// function greet(greeting) {
+//   if (!greeting) {
+//     throw new Error("There was no greeting given.");
+//   }
 
-  console.log(greeting);
-}
+//   console.log(greeting);
+// }
+
+// try {
+//   greet(null)
+// } catch (error){
+//   console.log("Hello World!")
+// }
